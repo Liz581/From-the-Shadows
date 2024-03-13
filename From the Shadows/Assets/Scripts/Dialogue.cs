212a6b0d2ba2b657606fed24f86acf3b9462c007
Lisset Rico <lrico5@uic.edu>
2024-03-13 09:45:45 -0500

    moveable capsule added and plane terrain

---
+++ From the Shadows/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    // initialize dialogue text variables
+    // Start is called before the first frame update
+
+    // Intro dialogue starts
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // wait until player crosses path with an NPC that interacts
+    }
+}
---
+++ From the Shadows/Assets/Scripts/NPC.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    // Initialize NPC objects
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // have NPCs on standby until they are prompted by the player
+    }
+}
